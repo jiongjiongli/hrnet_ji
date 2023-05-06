42a98ad7315105b9e19ee65485131fa01b87857d
Jiongjiong Li <jiongjiongai@outlook.com>
2023-05-06 08:16:45 -0500

    Update start_train.sh

--- start_train.sh
+++ start_train.sh
@@ -5,5 +5,5 @@ cp /home/data/1827/* /home/data/789/
 cp /home/data/1857/* /home/data/789/
 cp /home/data/1858/* /home/data/789/
 cp /home/data/1859/* /home/data/789/
-CUDA_VISIBLE_DEVICES=0 python /project/train/src_repo/train.py
-python /project/train/src_repo/toonnx.py
+CUDA_VISIBLE_DEVICES=0 python /project/train/src_repo/hrnet_ji/train.py
+python /project/train/src_repo/hrnet_ji/toonnx.py
