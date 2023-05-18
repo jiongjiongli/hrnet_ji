4749bf9790ec1e043bdfdaaeb62b29f189b65588
Jiongjiong Li <jiongjiongai@outlook.com>
2023-05-17 22:59:34 -0500

    1: Python predict model directly. 2:  argmax before toonnx. 3: Update batch size.

--- src/ji.py
+++ src/ji.py
@@ -6,53 +6,104 @@ import cv2
 import time
 import torch.nn.functional as F
 import torch
+from pathlib import Path
+
+import sys
+sys.path.append('/project/train/src_repo/hrnet_ji')
+from hrnet import HRnet_Segmentation
+
+
 def init():
     # h5_to_pb()
-    session = onnxruntime.InferenceSession("/project/train/models/seg_model.onnx",providers=['CUDAExecutionProvider'])
-    return session
+    # session = onnxruntime.InferenceSession("/project/train/models/seg_model.onnx",providers=['CUDAExecutionProvider'])
+    # return session
+    hrnet = HRnet_Segmentation(model_path ="/project/train/models/best_epoch_weights.pth",
+                               num_classes = 2,
+                               backbone = "hrnetv2_w32",
+                               input_shape=[512,512],
+                               mix_type=3)
+    return hrnet
 
 colors = [(0, 0, 0), (0, 0, 128)]
 def process_image(handle=None, input_image=None, args=None, **kwargs):
     model = handle
-    vis = True
+    vis = False
     img_size= [512,512]
 
     h, w, _ = input_image.shape
     input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-    
-    
+
     img = cv2.resize(input_image, img_size, cv2.INTER_LINEAR)
     img = np.array(img, np.float32)
     img -= np.array([123.675, 116.28, 103.53], np.float32)
     img /= np.array([58.395, 57.12, 57.375], np.float32)
     img = np.expand_dims(np.transpose(np.array(img), (2, 0, 1)), 0)
-    t0 = time.time()
-    y_pre = model.run([], {'images': img})[0][0]
-    t1 = time.time()
+    # t0 = time.time()
+    # y_pre = model.run([], {'images': img})[0][0]
+    # t1 = time.time()
 
-    y_pre = np.transpose(np.array(y_pre, np.float32), (1, 2, 0))
-    y_pre = torch.from_numpy(y_pre)
-    y_pre = y_pre.cpu()
-    y_pre = F.softmax(y_pre, dim=-1).cpu().numpy()
+    with torch.no_grad():
+        images = torch.from_numpy(img).cuda()
+
+        y_pre = model.net(images)[0]
+        y_pre = F.softmax(y_pre.permute(1, 2, 0), dim=-1).cpu().numpy()
+
+    # y_pre = np.transpose(np.array(y_pre, np.float32), (1, 2, 0))
+    # y_pre = torch.from_numpy(y_pre)
+    # y_pre = y_pre.cpu()
+    # y_pre = F.softmax(y_pre, dim=-1).cpu().numpy()
 
     y_pre = np.argmax(y_pre, axis=-1)
 
-    y_pre = np.squeeze(y_pre).astype(np.uint8)
+    y_pre = y_pre.astype(np.uint8)
+
+    # y_pre = np.squeeze(y_pre).astype(np.uint8)
 
     y_pre = cv2.resize(y_pre, (w, h), interpolation=cv2.INTER_NEAREST)
+
+    # input_image = Image.fromarray(input_image)
+    # y_pre = handle.detect_image(input_image)
+
     args = json.loads(args)
-    mask_output_path = args['mask_output_path']    
+    mask_output_path = args['mask_output_path']
     cv2.imwrite(mask_output_path, y_pre)
-    if vis:
-        seg_img = np.reshape(np.array(colors, np.uint8)[np.reshape(y_pre, [-1])], [h, w, -1])
-        render_map = seg_img * 0.8 + cv2.cvtColor(input_image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(mask_output_path.replace('.png','.jpg'),render_map)
-    return json.dumps({'mask': mask_output_path}, indent=4)
+    # if vis:
+    #     seg_img = np.reshape(np.array(colors, np.uint8)[np.reshape(y_pre, [-1])], [h, w, -1])
+    #     render_map = seg_img * 0.8 + cv2.cvtColor(input_image, cv2.COLOR_RGB2BGR)
+    #     cv2.imwrite(mask_output_path.replace('.png','.jpg'),render_map)
+    return json.dumps({'model_data': {'mask': mask_output_path}}, indent=4)
 
 
 if __name__ == '__main__':
+    model = init()
+    data_dir_path = r'/home/data'
+    data_dir_path = Path(data_dir_path)
+    data_file_paths = list(data_dir_path.rglob('*.jpg'))
+    print('total file count:', len(data_file_paths))
+    data_file_paths = data_file_paths[:20]
+    print('test file count:', len(data_file_paths))
+    process_image_durations = []
+    durations = []
 
-    input_image = cv2.imread("1.jpg")
-    for i in range(1):
-        result = process_image(init(), input_image,
-                           '{"mask_output_path": "onnx.png"}')
+    for data_file_path in data_file_paths:
+        input_image = cv2.imread(data_file_path.as_posix())
+        start_time = time.time()
+        result = process_image(model, input_image,
+                               '{"mask_output_path": "onnx.png"}')
+
+        end_time = time.time()
+        process_image_duration = end_time - start_time
+        process_image_durations.append(process_image_duration)
+
+        end_time = time.time()
+        duration = end_time - start_time
+        durations.append(duration)
+        print('duration:', duration)
+        print('process_image_duration:', process_image_duration)
+
+    duration_sum = sum(durations)
+    print('duration_average:', duration_sum / len(durations))
+    print('duration_sum:', duration_sum)
+    print('process_image_duration_average:', sum(process_image_durations) / len(process_image_durations))
+    print('process_image_duration_sum:', sum(process_image_durations))
+
--- toonnx.py
+++ toonnx.py
@@ -1,6 +1,93 @@
-from hrnet import HRnet_Segmentation
-simplify=True
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from torch import nn
+import onnx
+import onnxsim
 
-onnx_save_path = "/project/train/models/seg_model.onnx"
-hrnet = HRnet_Segmentation(model_path ="/project/train/models/best_epoch_weights.pth",num_classes = 2,backbone = "hrnetv2_w32",input_shape=[512,512] )
-hrnet.convert_to_onnx(simplify, onnx_save_path)
+from nets.hrnet import HRnet
+
+
+class HRnetPredict(HRnet):
+    def __init__(self, num_classes = 21, backbone = 'hrnetv2_w18', pretrained = False):
+        super(HRnetPredict, self).__init__(num_classes=num_classes, backbone=backbone, pretrained=pretrained)
+
+    def forward(self, inputs):
+        H, W = inputs.size(2), inputs.size(3)
+        x = self.backbone(inputs)
+
+        # Upsampling
+        x0_h, x0_w = x[0].size(2), x[0].size(3)
+        x1 = F.interpolate(x[1], size=(x0_h, x0_w), mode='bilinear', align_corners=True)
+        x2 = F.interpolate(x[2], size=(x0_h, x0_w), mode='bilinear', align_corners=True)
+        x3 = F.interpolate(x[3], size=(x0_h, x0_w), mode='bilinear', align_corners=True)
+
+        x = torch.cat([x[0], x1, x2, x3], 1)
+
+        x = self.last_layer(x)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+
+        # Shape: [B, C=1, H, W]
+        x = torch.argmax(x, dim=1, keepdim=True)
+        return x
+
+
+class HRnetConverter:
+    def __init__(self, model_path=None, num_classes = 21, backbone = 'hrnetv2_w18', pretrained = False, input_shape=None):
+        self.input_shape = input_shape
+        self.net = HRnetPredict(num_classes=num_classes, backbone=backbone, pretrained=pretrained)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.net.load_state_dict(torch.load(self.model_path, map_location=device))
+        self.net = self.net.eval()
+        print('{} model, and classes loaded.'.format(self.model_path))
+
+    def convert_to_onnx(self, simplify, onnx_save_path):
+        im = torch.zeros(1, 3, *self.input_shape).to('cpu')  # image size(1, 3, 512, 512) BCHW
+        input_layer_names = ["images"]
+        output_layer_names = ["output"]
+
+        # Export the model
+        print(f'Starting export with onnx {onnx.__version__}.')
+        torch.onnx.export(self.net,
+                          im,
+                          f=onnx_save_path,
+                          verbose=False,
+                          opset_version=12,
+                          training=torch.onnx.TrainingMode.EVAL,
+                          do_constant_folding=True,
+                          input_names=input_layer_names,
+                          output_names=output_layer_names,
+                          dynamic_axes=None)
+
+        # Checks
+        model_onnx = onnx.load(onnx_save_path)  # load onnx model
+        onnx.checker.check_model(model_onnx)  # check onnx model
+
+        # Simplify onnx
+        if simplify:
+            print(f'Simplifying with onnx-simplifier {onnxsim.__version__}.')
+            model_onnx, check = onnxsim.simplify(
+                model_onnx,
+                dynamic_input_shape=False,
+                input_shapes=None)
+            assert check, 'assert check failed'
+            onnx.save(model_onnx, model_path)
+
+        print('Onnx model save as {}'.format(model_path))
+
+
+def main():
+    simplify=True
+    onnx_save_path = "/project/train/models/seg_model.onnx"
+    model_path = "/project/train/models/best_epoch_weights.pth"
+    hrnet = HRnetConverter(model_path=model_path,
+                           num_classes=2,
+                           backbone="hrnetv2_w32",
+                           input_shape=[512,512])
+    hrnet.convert_to_onnx(simplify, onnx_save_path)
+
+
+if __name__ == '__main__':
+    main()
